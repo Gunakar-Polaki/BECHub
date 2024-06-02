afc5015eac2368f4abe6d3c2002c64e2af4afc9f
Polaki Gunakar <131868369+Gunakar-Polaki@users.noreply.github.com>
2024-06-02 13:18:24 +0530

    Create background.js

---
+++ assets/js/background.js
@@ -0,0 +1,17 @@
+const images = [
+    'assets/images/bec1.jpg',
+    'assets/images/bec2.jpg',
+    'assets/images/bec3.jpg',
+    'assets/images/bec4.jpg',
+    'assets/images/bec5.jpg',
+    'assets/images/bec6.jpg',
+];
+
+let currentIndex = 0;
+
+function changeBackground() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.querySelector('.home-bg').style.backgroundImage = `url(${images[currentIndex]})`;
+}
+
+setInterval(changeBackground, 3000);
