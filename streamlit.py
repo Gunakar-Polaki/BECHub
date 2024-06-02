df95d00d4c3180ee292d101b99527f5caa05c3a1
Polaki Gunakar <131868369+Gunakar-Polaki@users.noreply.github.com>
2024-06-02 14:21:31 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,17 +1,8 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 st.title("BEC Hub")
 
 if st.button('Redirect to BEC Hub'):
-    # Use streamlit's components to run the JS script for redirection
-    components.html(
-        """
-        <script type="text/javascript">
-            window.location = 'https://gunakar-polaki.github.io/BECHub/index.html';
-        </script>
-        """,
-        height=0,
-    )
+    st.markdown('<meta http-equiv="refresh" content="0; url=https://gunakar-polaki.github.io/BECHub/index.html">', unsafe_allow_html=True)
 
 st.write("Click the button above to be redirected to the BEC Hub Home page.")
