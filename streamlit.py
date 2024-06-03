fcf203836982925761b80fd80b1e828ace8fb0bd
Polaki Gunakar <131868369+Gunakar-Polaki@users.noreply.github.com>
2024-06-03 10:26:37 +0530

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
 st.title("BEC Hub")
+st.markdown('<meta http-equiv="refresh" content="0; url=https://gunakar-polaki.github.io/BECHub/index.html">', unsafe_allow_html=True)
 
-if st.button('Redirect to BEC Hub'):
-    st.markdown('<meta http-equiv="refresh" content="0; url=https://gunakar-polaki.github.io/BECHub/index.html">', unsafe_allow_html=True)
-
-st.write("Click the button above to be redirected to the BEC Hub Home page.")
