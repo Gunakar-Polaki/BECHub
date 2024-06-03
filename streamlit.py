6cf96b0bf5edfd3edbfc08132669bca95960e0ca
Polaki Gunakar <131868369+Gunakar-Polaki@users.noreply.github.com>
2024-06-03 11:01:20 +0530

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.markdown('<meta http-equiv="refresh" content="0; url=https://gunakar-polaki.github.io/BECHub/">', unsafe_allow_html=True)
+st.markdown('<meta http-equiv="refresh" content="0; url=https://gunakar-polaki.github.io/BECHub/index.html">', unsafe_allow_html=True)
 
